583a422b2c8e3444ae0016af9988762dc402a108
jawil <609514766@qq.com>
2017-08-31 01:30:29 +0800

    睡觉

--- chrome/contentscript.js
+++ chrome/contentscript.js
@@ -2,7 +2,6 @@ let container =
     document.querySelector('.markdown-body')
 
 if (container) {
-
     let current = { index: 0, Li: '' },
         isClick = false,
         titleArr = container.querySelectorAll('h1,h2,h3,h4,h5,h6'),
