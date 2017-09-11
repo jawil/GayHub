592016b64178faa8768020de5d55e48c373f5285
jawil <609514766@qq.com>
2017-09-11 11:22:35 +0800

    fix some bugs

--- src/components/sidebarNav.js
+++ src/components/sidebarNav.js
@@ -1,10 +1,9 @@
 /* 生成侧边栏sidebar的HTML */
 import { generateContainerHTML } from 'utils/generatePage'
-import parentNotRoll from 'libs/parentNotRoll'
-import Pjax from 'pjax'
 import { toggleBtn, urlChangeEvent } from 'utils/setIconStyle'
 import { getUrlParam, initDOM } from 'utils/sideBarHelp'
-
+import parentNotRoll from 'libs/parentNotRoll'
+import Pjax from 'pjax'
 
 const sideBarMain = generateContainerHTML()
 
