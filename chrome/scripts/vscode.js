2ad622ec245abdadd9ce40e944a2097d9dfb0725
jawil <609514766@qq.com>
2017-09-11 17:47:03 +0800

    更换几张图标

---
+++ chrome/scripts/vscode.js
@@ -0,0 +1,25 @@
+let path = chrome.extension.getURL('vs-dev/vs')
+
+require.config({ paths: { 'vs': path } })
+
+
+let entrance = chrome.extension.getURL('vs-dev/vs/editor/editor.main.js')
+
+
+document.body.innerHTML = ''
+
+
+require(['vs/editor/editor.main'], function(ele) {
+
+    console.log(22)
+
+    var editor = monaco.editor.create(document.body, {
+        value: [
+            'function x() {',
+            '\tconsole.log("Hello world!");',
+            '}'
+        ].join('\n'),
+        language: 'javascript'
+
+    });
+})
