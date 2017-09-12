635af6b58c356416e388ceb64d87f7d6785ce641
jawil <609514766@qq.com>
2017-09-12 14:48:43 +0800

    fix some bugs

--- src/components/sidebarNav.js
+++ src/components/sidebarNav.js
@@ -18,6 +18,7 @@ module.exports = function() {
     const fileWrap = document.querySelectorAll('.file-wrap,.file')
 
     if (fileWrap.length) {
+
         /* 获取参数 */
         const oParam = getUrlParam()
 
