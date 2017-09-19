a3e369d48f27b40a58fa58fb757558773670f8cd
axetroy <troy450409405@gmail.com>
2017-09-19 14:27:10 +0800

    style(webpack): Format the code with Prettier@1.7.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
 // 设置输入和输出根目录
 const ROOT_PATH = path.resolve(__dirname);
-const APP_PATH = path.resolve(ROOT_PATH, "src");
-const BUILD_PATH = path.resolve(ROOT_PATH, "chrome/scripts");
+const APP_PATH = path.resolve(ROOT_PATH, 'src');
+const BUILD_PATH = path.resolve(ROOT_PATH, 'chrome/scripts');
 
 //获取环境
 const env = process.env.NODE_ENV;
@@ -12,43 +12,43 @@ const env = process.env.NODE_ENV;
 module.exports = {
   /* source-map */
   devtool:
-    env === "production" ? "hidden-source-map" : "cheap-module-eval-source-map",
+    env === 'production' ? 'hidden-source-map' : 'cheap-module-eval-source-map',
   entry: {
-    app: "./src/index.js"
+    app: './src/index.js'
   },
   output: {
     path: BUILD_PATH, // 编译到当前目录
-    filename: "contentscript.js",
-    sourceMapFilename: "contentscript.js.map"
+    filename: 'contentscript.js',
+    sourceMapFilename: 'contentscript.js.map'
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/, // 用babel编译jsx和es6
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
           cacheDirectory: true,
-          presets: ["es2015"],
-          plugins: [["transform-runtime"]]
+          presets: ['es2015'],
+          plugins: [['transform-runtime']]
         }
       }
     ]
   },
   resolve: {
-    extensions: [".js"],
+    extensions: ['.js'],
     alias: {
-      components: path.resolve(APP_PATH, "./components"),
-      utils: path.resolve(APP_PATH, "./utils"),
-      libs: path.resolve(APP_PATH, "./libs")
+      components: path.resolve(APP_PATH, './components'),
+      utils: path.resolve(APP_PATH, './utils'),
+      libs: path.resolve(APP_PATH, './libs')
     }
   },
   plugins: [],
-  watch: env === "development"
+  watch: env === 'development'
 };
 
 switch (env) {
-  case "production":
+  case 'production':
     module.exports.plugins = (module.exports.plugins || []).concat([
       new webpack.optimize.UglifyJsPlugin({
         compress: {
