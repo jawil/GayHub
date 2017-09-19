d323cca2c60eaf0b4e2570d4eb0fe0886a86039d
jawil <609514766@qq.com>
2017-09-19 14:40:12 +0800

    Refactor Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         options: {
           cacheDirectory: true,
           presets: ['es2015'],
-          plugins: [['transform-runtime']]
+          plugins: ['transform-runtime']
         }
       }
     ]
